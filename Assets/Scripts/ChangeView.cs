9c1a101f96ddcc483df98ff57758158f5e6fc08c
rafatutankamon <rafa.quevedo@yahoo.com.br>
2016-09-03 02:56:49 -0300

    bug fixes
    correção no comportamento do prefab das categorias, teste com paginação

---
+++ Assets/Scripts/ChangeView.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeView : MonoBehaviour {
+
+    public GameObject container;
+	// Use this for initialization
+
+    private Vector3 _initialPosition;
+	void Start () {
+        _initialPosition = container.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void BtnClick()
+    {
+        container.transform.position = Vector3.Lerp(container.transform.position, new Vector3(container.transform.position.x - 372f, container.transform.position.y, 0), 3f * Time.fixedTime); 
+    }
+    public void RestartPosition()
+    {
+       container.transform.position = _initialPosition;
+    }
+}
